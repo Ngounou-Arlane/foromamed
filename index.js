881be4ff090a751c1e46c434ac37f6c5193a723b
Ngounou Arlane Otantik Nuna <authenticnuna123@gmail.com>
2025-03-11 06:24:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ const upload = multer({ storage });
 app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
 
 // Endpoint to handle file uploads
+// app.post('/upload', upload.single('image'), (req, res) => {
+//   if (!req.file) {
+//     return res.status(400).json({ error: 'No file uploaded' });
+//   }
+
+
+
+// Endpoint to handle file uploads
 app.post('/upload', upload.single('image'), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No file uploaded' });
@@ -38,7 +46,8 @@ app.post('/upload', upload.single('image'), (req, res) => {
   res.json({ fileUrl });
 });
 
+
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  // console.log(`Server running at http://localhost:${port}`);
 });
